aabccc14e134473820a627c26023e756f9399921
SebasKHE <sebasttcaicedo@gmail.com>
2025-02-11 15:44:43 -0500

    changes

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div>
+            <footer>
+                <div>
+                    <p>Sebastian Caicedo Animit CEO</p>
+                </div>
+                
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
